61f7c893d6d933fe78689ba5b35670ed5e2b071c
Emilien Domenge <emilien.domenge-heritier@abtasty.com>
2019-12-10 18:01:24 +0100

    Updates

---
+++ precache-manifest.aff1f0eade26089871450c32353f2c33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "617147f4573c76a827a0156403765c32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4b4bd896f5459115514",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "1c97d6a1e070211883f9",
+    "url": "/static/js/2.96c6fbb6.chunk.js"
+  },
+  {
+    "revision": "a4b4bd896f5459115514",
+    "url": "/static/js/main.0714bff5.chunk.js"
+  },
+  {
+    "revision": "6858e678c27d3816988a",
+    "url": "/static/js/runtime-main.a8580334.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
