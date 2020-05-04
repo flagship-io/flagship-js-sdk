edd629fa299a9c13b589655448bfafa33b38f4a3
Emilien Domenge <emilien.domenge-heritier@abtasty.com>
2020-05-04 17:44:07 +0200

    Minor change

--- examples/api-server/services/flagship.js
+++ examples/api-server/services/flagship.js
@@ -1,6 +1,5 @@
+var flagship = require('@flagship.io/js-sdk');
 
-var flagship = require('@flagship.io/js-sdk')
-
-const sdk = flagship.initSdk("bn1ab7m56qolupi5sa0g", /* some settings */);
+const sdk = flagship.start('bn1ab7m56qolupi5sa0g' /* some settings */);
 
 module.exports = sdk;
