a486c67223546daa5ef6bec29219c74676bbeb9d
Madi-Ji <Madi-Ji@users.noreply.github.com>
2020-10-15 20:51:36 +0200

    File renaming - Standalone
    - Removing CDN mention
    - Stadalone, one single word

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const nodeConfig = require('./webpack/config.node.js');
 const browserConfig = require('./webpack/config.browser.js');
 const reactNativeConfig = require('./webpack/config.reactNative.js');
-const standAloneConfig = require('./webpack/config.cdn.js');
+const standAloneConfig = require('./webpack/config.standalone.js');
 
-module.exports = [nodeConfig, browserConfig, reactNativeConfig, standAloneConfig];
+module.exports = [nodeConfig, browserConfig, reactNativeConfig, standaloneConfig];
