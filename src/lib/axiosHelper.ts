d1849f4110f22a522a29e5d8b7f458c4a9b2958c
Merveille KITOKO <merkitoko@gmail.com>
2022-01-12 13:33:05 +0100

    fix webpack target web issue

--- src/lib/axiosHelper.ts
+++ src/lib/axiosHelper.ts
@@ -2,10 +2,11 @@ import axios from 'axios';
 import { Agent as HttpsAgent } from 'https';
 import { Agent as HttpAgent } from 'http';
 
-const instance = axios.create({
-    httpAgent: new HttpAgent({ keepAlive: true }),
-    httpsAgent: new HttpsAgent({ keepAlive: true })
-});
+const instance = axios.create();
 
+if (window == undefined) {
+    instance.defaults.httpAgent = new HttpAgent({ keepAlive: true });
+    instance.defaults.httpsAgent = new HttpsAgent({ keepAlive: true });
+}
 export const defaultAxios = axios;
 export default instance;
